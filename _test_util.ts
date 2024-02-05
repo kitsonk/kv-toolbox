43bce065a8dfba66ba6c67f515157e10acc46da7
Kitson Kelly <me@kitsonkelly.com>
2024-02-06 10:37:00 +1100

    chore: update std for tests

--- _test_util.ts
+++ _test_util.ts
@@ -1,11 +1,11 @@
-import { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
-export { delay } from "https://deno.land/std@0.203.0/async/delay.ts";
-export { assert } from "https://deno.land/std@0.203.0/assert/assert.ts";
-export { assertEquals } from "https://deno.land/std@0.203.0/assert/assert_equals.ts";
+import { assert } from "https://deno.land/std@0.214.0/assert/assert.ts";
+export { delay } from "https://deno.land/std@0.214.0/async/delay.ts";
+export { assert } from "https://deno.land/std@0.214.0/assert/assert.ts";
+export { assertEquals } from "https://deno.land/std@0.214.0/assert/assert_equals.ts";
 export {
   assertNotEquals,
-} from "https://deno.land/std@0.203.0/assert/assert_not_equals.ts";
-export { timingSafeEqual } from "https://deno.land/std@0.203.0/crypto/timing_safe_equal.ts";
+} from "https://deno.land/std@0.214.0/assert/assert_not_equals.ts";
+export { timingSafeEqual } from "https://deno.land/std@0.214.0/crypto/timing_safe_equal.ts";
 
 let kv: Deno.Kv | undefined;
 let path: string | undefined;
