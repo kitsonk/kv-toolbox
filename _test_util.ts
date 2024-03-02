0e53de4d620b613d383935c1cfa6009b4ac96e3c
Kitson Kelly <me@kitsonkelly.com>
2024-03-02 20:20:24 +1100

    chore: update to std 0.218.2

--- _test_util.ts
+++ _test_util.ts
@@ -1,11 +1,11 @@
-import { assert } from "https://deno.land/std@0.217.0/assert/assert.ts";
-export { delay } from "https://deno.land/std@0.217.0/async/delay.ts";
-export { assert } from "https://deno.land/std@0.217.0/assert/assert.ts";
-export { assertEquals } from "https://deno.land/std@0.217.0/assert/assert_equals.ts";
+import { assert } from "https://deno.land/std@0.218.2/assert/assert.ts";
+export { delay } from "https://deno.land/std@0.218.2/async/delay.ts";
+export { assert } from "https://deno.land/std@0.218.2/assert/assert.ts";
+export { assertEquals } from "https://deno.land/std@0.218.2/assert/assert_equals.ts";
 export {
   assertNotEquals,
-} from "https://deno.land/std@0.217.0/assert/assert_not_equals.ts";
-export { timingSafeEqual } from "https://deno.land/std@0.217.0/crypto/timing_safe_equal.ts";
+} from "https://deno.land/std@0.218.2/assert/assert_not_equals.ts";
+export { timingSafeEqual } from "https://deno.land/std@0.218.2/crypto/timing_safe_equal.ts";
 
 let kv: Deno.Kv | undefined;
 let path: string | undefined;
